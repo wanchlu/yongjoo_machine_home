e6d063cda7fa9445832958265a1bb477af393541
Wanchen <luwanchen@gmail.com>
2012-07-22 22:57:41 -0400

    e

---
+++ work/zhu/batch_zhu.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+
+use IO::Handle;
+
+autoflush STDOUT 1;
+my $trainfile = shift;
+my $testfile = shift;
+my $testsize = shift;
+
+`shuffle.pl $testfile | head -n$testsize >$testfile.$testsize`;
+$testfile = "$testfile.$testsize";
+
+my $MAXIT = 1;
+my $tag = localtime(time);
+$tag =~ s/\W+//g;
+
+#for my $trainsize (5, 10, 15, 20, 25, 50, 75, 100, 150, 200, 250, 300, 350, 400, 450, 500) {
+#for my $trainsize (5, 10, 15, 20, 25 ) {
+`mkdir $tag`;
+for my $trainsize (2, 4, 6, 8, 10, 12, 14, 16, 18, 20) {
+  print "Size: $trainsize\n";
+  my $v1 = 0;
+  for (my $iter = 0; $iter < $MAXIT; $iter++) {
+
+  `./data/convert_to_zhu_format.pl $trainfile $trainsize $testfile`;
+  my $totalsize = $trainsize + $testsize;
+
+  my $f1 = `./zhu -graph $trainfile.$trainsize.sim -trainlabels $trainfile.$trainsize.labels -testlabels $testfile.labels -out zhu.out -classes 2`;
+
+  chomp($f1);
+
+  $f1 =~ m/.*:\s+(.*)$/;
+  $v1 += $1;
+
+  print "$f1\n";
+  `cp $trainfile.$trainsize.sim $tag/$trainsize.sim`;
+  `cp zhu.out $tag/$trainsize.out`;
+  `echo "$f1" > $tag/$trainsize.acc`;
+  `cp $trainfile.$trainsize.shuffle $tag/$trainsize.train`;
+  }
+  `cp $testfile $tag/test`;
+
+  print "\n averages: ";
+  print $v1 / $MAXIT, "\n";
+
+  print "\n";
+  #sleep (1);
+}
---
+++ work/zhu/zhu.cpp
@@ -0,0 +1,241 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+typedef struct edge {
+  unsigned int ids[2];
+  float weight;
+} edge_t;
+
+
+typedef struct node {
+  float *old_values;
+  float *new_values;
+  float influx;
+  char labeled;
+}node_t;
+
+unsigned int n_edges=0, n_nodes=0;
+
+short n_classes;
+
+char *train_label_file, *test_label_file, *sim_file, *output_file;
+
+node_t *nodes;
+edge_t *edges;
+
+void process_args(int argc, char **argv);
+void read_labels();
+void read_edges();
+float propagate_labels();
+void output_labels ();
+
+void determine_limits () {
+  FILE *fp;
+  fp = fopen (sim_file, "r");
+  unsigned int n1, n2;
+  float weight;
+
+  while ((fscanf(fp, "%d %d %f\n", &n1, &n2, &weight)) != EOF) {
+    if (weight <= 0.0) continue;
+    n_edges++;
+    if (n1 > n_nodes) {
+      n_nodes = n1;
+    }
+    if (n2 > n_nodes) {
+      n_nodes = n2;
+    }
+  }
+  fclose (fp);
+}
+
+
+/*** MAIN FUNCTION ***/
+int main (int argc, char **argv) {
+
+  // process the command line
+  process_args (argc, argv);
+
+  determine_limits();
+
+  // check if there is something wrong in the command
+  if (n_nodes <= 0 || n_edges <= 0) {
+    fprintf (stderr, "Incorrect or missing node numbers\n");
+    exit (1);
+  }
+
+  fprintf (stderr, "allocating edges..\n");
+  edges = (edge_t *) malloc(n_edges * sizeof(edge_t));
+  read_edges();
+
+  fprintf (stderr, "allocating nodes..\n");
+  nodes = (node_t *) malloc(n_nodes * sizeof(node_t));
+  memset (nodes, 0, n_nodes * sizeof(node_t));
+  for (unsigned int i=0; i < n_nodes; i++) {
+    nodes[i].old_values = (float *) malloc (n_classes * sizeof(float));
+    nodes[i].new_values = (float *) malloc (n_classes * sizeof(float));
+  }
+
+  read_labels();
+
+  for (unsigned int i=0; i < n_nodes; i++) {
+    for (short j=0; j < n_classes; j++) {
+      if (!nodes[i].labeled)
+         nodes[i].old_values[j] = 1.0 / n_classes;
+      nodes[i].new_values[j] = 0;
+    }
+  }
+
+  // compute the influx (weighted degree) of each node
+  for (unsigned int i=0; i < n_edges; i++) {
+    nodes[edges[i].ids[0]].influx += edges[i].weight;
+    nodes[edges[i].ids[1]].influx += edges[i].weight;
+  }
+
+  float max_error;
+
+  do {
+    max_error = propagate_labels();
+    fprintf (stderr, "error: %f\n", max_error);
+  } while (max_error > 0.001);
+
+  output_labels();
+
+  return 0;
+}
+
+
+void output_labels () {
+  FILE *gp;
+  gp = fopen (test_label_file, "r");
+  int total=0,correct=0;
+
+  for (unsigned int i=0; i < n_nodes; i++) {
+    if (!nodes[i].labeled) {
+      short label;
+      fscanf (gp, "%hd\n", &label);
+      total++;
+      if (nodes[i].old_values[0] < nodes[i].old_values[1])
+      {
+       if (label == 1)
+         correct++;
+      }
+      else
+      {
+       if (label == 0)
+         correct++;
+      }
+    }
+  }
+  fclose (gp);
+
+  printf ("Accuracy: %d / %d : %f\n",
+                   correct, total, (correct+0.0)/total);
+}
+
+
+
+float propagate_labels() {
+
+  float max_error = 0;
+
+  for (unsigned int i=0; i < n_edges; i++) {
+    if (!(nodes[edges[i].ids[0]].labeled))
+      for (short j=0; j < n_classes; j++)
+        nodes[edges[i].ids[0]].new_values[j] += nodes[edges[i].ids[1]].old_values[j] * edges[i].weight;
+    if (!(nodes[edges[i].ids[1]].labeled))
+      for (short j=0; j < n_classes; j++)
+        nodes[edges[i].ids[1]].new_values[j] += nodes[edges[i].ids[0]].old_values[j] * edges[i].weight;
+  }
+
+  for (unsigned int i=0; i < n_nodes; i++) {
+    if ((!nodes[i].labeled) && nodes[i].influx > 0) {
+      for (short j=0; j < n_classes; j++) {
+        float new_value = nodes[i].new_values[j] / nodes[i].influx;
+        float error = new_value - nodes[i].old_values[j];
+        if (error < 0) error = -error;
+        if (error > max_error) max_error = error;
+        nodes[i].old_values[j] = new_value;
+        nodes[i].new_values[j] = 0;
+      }
+    }
+  }
+
+  return max_error;
+
+}
+
+
+
+// read edge information from file
+void read_edges() {
+  FILE *fp;
+  unsigned int count=0, n1, n2;
+  float weight;
+  fp = fopen (sim_file, "r");
+
+  while ((fscanf(fp, "%d %d %f\n", &n1, &n2, &weight)) != EOF) {
+    if (weight > 0.0) {
+      edges[count].ids[0] = n1-1;
+      edges[count].ids[1] = n2-1;
+      edges[count].weight = weight;
+      count++;
+    }
+  }
+  fclose (fp);
+}
+
+
+// read labeled nodes information from file
+void read_labels() {
+  FILE *fp;
+  unsigned int n1;
+  short c;
+  fp = fopen (train_label_file, "r");
+
+  while ((fscanf(fp, "%d %hd\n", &n1, &c)) != EOF) {
+    nodes[n1-1].old_values[c] = 1.0;
+    nodes[n1-1].labeled = 1;
+   // fprintf (stderr, "label: %hd\n", c);
+  }
+  fclose(fp);
+}
+
+
+void process_args(int argc, char **argv) {
+  for (int argi = 1; argi < argc; ) {
+    if (strcmp(argv[argi], "-out") == 0) {
+      if (argc <= argi + 1) goto usage;
+      output_file = strdup(argv[argi+1]);
+      argi += 2;
+    }
+    else if (strcmp(argv[argi], "-graph") == 0) {
+      if (argc <= argi + 1) goto usage;
+      sim_file = strdup(argv[argi+1]);
+      argi += 2;
+    }
+    else if (strcmp(argv[argi], "-trainlabels") == 0) {
+      if (argc <= argi + 1) goto usage;
+      train_label_file = strdup(argv[argi+1]);
+      argi += 2;
+    }
+    else if (strcmp(argv[argi], "-testlabels") == 0) {
+      if (argc <= argi + 1) goto usage;
+      test_label_file = strdup(argv[argi+1]);
+      argi += 2;
+    }
+    else if (strcmp(argv[argi], "-classes") == 0) {
+      if (argc <= argi + 1) goto usage;
+      n_classes = atoi(argv[argi+1]);
+      argi += 2;
+      fprintf (stderr, "classes: %hd\n", n_classes);
+    }
+  }
+  return;
+  usage:
+  fprintf(stderr, "Incorrect command line!!!\n");
+  exit(1);
+}
+
